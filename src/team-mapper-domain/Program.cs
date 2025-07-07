735cf94fa80f7633281831814f5c38f31962aadf
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-07 17:47:34 +0200

    wip: team-mapper-domain project added

---
+++ src/team-mapper-domain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace team_mapper_domain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
