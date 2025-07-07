0fc764921b3634ae875890012f93e7805e39fc47
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-07 19:13:40 +0200

    wip: appdbcontext added

---
+++ src/team-mapper-infrastructure/Infrastructure/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace team_mapper_infrastructure.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
