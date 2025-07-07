96469216460010a37dc8556247979d8babd3fcfc
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-07 17:46:42 +0200

    src: team mapper infrastrcuture project added

---
+++ src/team-mapper-infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace team_mapper_infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
