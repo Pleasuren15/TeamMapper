8d8c074e3227b9d659961b60676d6ebaa066108a
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-18 11:24:08 +0200

    src: more entity framework setup

--- src/team-mapper-api/Program.cs
+++ src/team-mapper-api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using team_mapper_infrastructure.Infrastructure;
+using team_mapper_infrastructure.RepositoryPattern;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("TeamMapperDb")));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<TaskService>();
 
 builder.Services.AddCors(options =>
 {
---
+++ src/team-mapper-infrastructure/RepositoryPattern/TaskService.cs
@@ -0,0 +1,12 @@
+﻿namespace team_mapper_infrastructure.RepositoryPattern
+{
+    public class TaskService(IRepository<team_mapper_domain.Models.Task> taskRepository)
+    {
+        private readonly IRepository<team_mapper_domain.Models.Task> _taskRepository = taskRepository;
+
+        public async Task<IEnumerable<team_mapper_domain.Models.Task>> GetTasksProductsAsync()
+        {
+            return await _taskRepository.GetAllAsync();
+        }
+    }
+}
