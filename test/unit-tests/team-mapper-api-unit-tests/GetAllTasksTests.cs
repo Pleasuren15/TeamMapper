1c7df016e6201e0c38c46cff6d240b752622c4f8
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-20 10:06:17 +0200

    wip: init tests covering core functionality added

--- test/unit-tests/team-mapper-api-unit-tests/GetAllTasksTests.cs
+++ test/unit-tests/team-mapper-api-unit-tests/GetAllTasksTests.cs
@@ -1,6 +1,7 @@
 ﻿using AwesomeAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using team_mapper_api.Endpoints.Tasks;
 using team_mapper_shared_utilities.ResponseFactories;
 using team_mapper_shared_utilities.Stubs;
@@ -36,6 +37,34 @@ public class GetAllTasksTests
         var controllerResponse = results as ObjectResult;
         var controllerResponseValue = controllerResponse!.Value as List<team_mapper_domain.Models.Task>;
         controllerResponseValue!.Count.Should().Be(randomTasks.Count());
+    }
+
+    [Test]
+    public async Task GivenNoTasksExists_WhenCallingGetAllTasksAsync_ThenShouldReturnEmptyList()
+    {
+        // Arrange
+        _tasksSubstitute.Repository.GetAllAsync().Returns(new List<team_mapper_domain.Models.Task>());
+
+        // Act
+        var results = await _systemUnderTest.GetAllTasksAsync();
+
+        // Assert
+        var controllerResponse = results as ObjectResult;
+        var controllerResponseValue = controllerResponse!.Value as List<team_mapper_domain.Models.Task>;
+        controllerResponseValue!.Count.Should().Be(0);
+    }
+
+    [Test]
+    public void GivenGetAllThrowsException_WhenCallingGetAllTasksAsync_ThenShouldReturnInternalServerError()
+    {
+        // Arrange
+        _tasksSubstitute.Repository.GetAllAsync().ThrowsAsync(new Exception("Database error"));
+
+        // Act
+        var exception = Assert.ThrowsAsync<Exception>(async () => await _systemUnderTest.GetAllTasksAsync());
+
+        // Assert
+        exception.InnerException.Should().NotBeNull();
     }
 
     [TearDown]
