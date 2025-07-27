a067d323b9238847b0569e383ec1b6f81856476b
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-27 09:33:16 +0200

    wip: more migrations added

---
+++ src/team-mapper-infrastructure/Migrations/20250727072743_LastTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace team_mapper_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "WorkItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WorkItems",
+                table: "WorkItems",
+                column: "WorkItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WorkItems",
+                table: "WorkItems");
+
+            migrationBuilder.RenameTable(
+                name: "WorkItems",
+                newName: "Tasks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "WorkItemId");
+        }
+    }
+}
