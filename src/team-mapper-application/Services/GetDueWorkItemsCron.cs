2c2592b8de6af7f737f685c35768a2d17ae39003
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-28 17:04:10 +0200

    cron job skeleton added

--- src/team-mapper-application/Services/GetDueWorkItemsCron.cs
+++ src/team-mapper-application/Services/GetDueWorkItemsCron.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace team_mapper_application.Services;
 
-public class GetDueWorkItemsCron : IHostedService
+public class GetDueWorkItemsCron(ILogger<GetDueWorkItemsCron> logger) : IHostedService, IDisposable
 {
+    private Timer _timer;
+    private ILogger<GetDueWorkItemsCron> _logger = logger;
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        _timer = new Timer(ExecuteWork, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(5));
         throw new NotImplementedException();
     }
 
@@ -13,4 +18,11 @@ public class GetDueWorkItemsCron : IHostedService
     {
         throw new NotImplementedException();
     }
+
+    public void ExecuteWork(object state)
+    {
+        _logger.LogInformation("Cron Job Set up");
+    }
+
+    public void Dispose() => _logger.LogInformation("Disposing GetDueWorkItemsCron Resources.");
 }
