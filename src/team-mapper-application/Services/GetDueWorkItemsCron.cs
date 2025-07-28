6f2c6f8af97b5837c352abefd5bab5ba4d33d900
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-07-28 16:52:28 +0200

    wip: cron class added

---
+++ src/team-mapper-application/Services/GetDueWorkItemsCron.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace team_mapper_application.Services;
+
+public class GetDueWorkItemsCron : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
