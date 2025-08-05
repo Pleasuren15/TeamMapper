a146ef32816e0f7eab71cd5d2f17630445ffb393
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-05 17:54:36 +0200

    wip: rabbitmq valyues added to appsettings

---
+++ src/team-mapper-application/Interfaces/IRabbitMqWrapper.cs
@@ -0,0 +1,6 @@
+﻿namespace team_mapper_application.Interfaces;
+
+internal interface IRabbitMqWrapper
+{
+    Task PublishMessagesAsync();
+}
---
+++ src/team-mapper-application/Services/RabbitMqWrapper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using team_mapper_application.Interfaces;
+
+namespace team_mapper_application.Services;
+
+public class RabbitMqWrapper(IConfiguration configuration) : IRabbitMqWrapper
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public Task PublishMessagesAsync()
+    {
+        var hostConnectionString = _configuration.GetConnectionString("RabbitMqHost");
+        var clintentProvidedName = _configuration["RabbitMq:ClientProvideName"];
+        var exchangeName = _configuration["RabbitMq:ExchangeName"];
+        var queueName = _configuration["RabbitMq:QueueName"];
+        var routingKey = _configuration["RabbitMq:RoutingKey"];
+
+        var connectionFactory = new ConnectionFactory()
+        {
+            Uri = new Uri(hostConnectionString!),
+            ClientProvidedName = clintentProvidedName
+        };
+
+        return Task.CompletedTask''
+    }
+}
