a920f7649890decbe736b2e6701c53b18bd9b309
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-05 18:05:13 +0200

    wip: PublishMessagesAsync for messages added

--- src/team-mapper-application/Services/RabbitMqWrapper.cs
+++ src/team-mapper-application/Services/RabbitMqWrapper.cs
@@ -9,7 +9,7 @@ public class RabbitMqWrapper(IConfiguration configuration) : IRabbitMqWrapper
 {
     private readonly IConfiguration _configuration = configuration;
 
-    public Task PublishMessagesAsync()
+    public async Task PublishMessagesAsync()
     {
         var hostConnectionString = Guard.Against.NullOrEmpty(input: _configuration.GetConnectionString("RabbitMqHost"));
         var clintentProvidedName = Guard.Against.NullOrEmpty(input: _configuration["RabbitMq:ClientProvideName"]);
@@ -22,7 +22,14 @@ public class RabbitMqWrapper(IConfiguration configuration) : IRabbitMqWrapper
             Uri = new Uri(hostConnectionString!),
             ClientProvidedName = clintentProvidedName
         };
+        using var connection = await connectionFactory.CreateConnectionAsync();
 
-        return Task.CompletedTask;
+        using var channel = await connection.CreateChannelAsync();
+        await channel.ExchangeDeclareAsync(exchange: exchangeName, ExchangeType.Direct);
+        await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+        await channel.QueueBindAsync(queue: queueName, exchange: exchangeName, routingKey: routingKey);
+
+        byte[] messageBody = System.Text.Encoding.UTF8.GetBytes(s: "Hello, RabbitMQ!");
+        await channel.BasicPublishAsync(exchangeName, routingKey, messageBody);
     }
 }
