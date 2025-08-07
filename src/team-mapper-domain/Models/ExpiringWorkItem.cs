ccab1f9e578bbc6856aca7411f3a9f45f2dae16e
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-07 18:32:39 +0200

    src: ExpiringWorkItem model added for notifications that are soon to expire

---
+++ src/team-mapper-domain/Models/ExpiringWorkItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace team_mapper_domain.Models;
+
+public class ExpiringWorkItem
+{
+    [JsonPropertyName(nameof(ExpiringWorkItemId))]
+    public Guid ExpiringWorkItemId { get; set; }
+
+    [JsonPropertyName(nameof(EndDate))]
+    public DateTime EndDate { get; set; }
+
+    [JsonPropertyName(nameof(IsNotificationSent))]
+    public bool IsNotificationSent { get; set; } = false;
+
+    [Required]
+    [JsonPropertyName(nameof(WorkItemId))]
+    public Guid WorkItemId { get; set; }
+
+    [JsonPropertyName(nameof(WorkItem))]
+    public WorkItem? WorkItem { get; set; }
+}
