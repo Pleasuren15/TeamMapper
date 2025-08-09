c3523ec01700c3aae3f9892745fd67328e32d232
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-09 17:00:25 +0200

    wip: infrastructure services added

--- src/team-mapper-application/TeamMembersManager.cs
+++ src/team-mapper-application/TeamMembersManager.cs
@@ -1,12 +1,29 @@
-﻿using team_mapper_application.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using team_mapper_application.Interfaces;
 using team_mapper_domain.Models;
 
 namespace team_mapper_application;
 
-public class TeamMembersManager : ITeamMembersManager
+public class TeamMembersManager(ILogger<WorkItemsManager> logger) : ITeamMembersManager
 {
+    private readonly ILogger<WorkItemsManager> _logger = logger;
+
     public async Task<IEnumerable<TeamMember>> GetAllTeamMembersAsync(Guid correlationId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _logger.LogInformation("GetAllTeamMembersAsync Start: CorrelationId {@CorrelationId}", correlationId);
+
+            return default!;
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError("GetAllTeamMembersAsync Error: Message {@Message}", exception.Message);
+            throw;
+        }
+        finally
+        {
+            _logger.LogInformation("GetAllTeamMembersAsync Start: CorrelationId {@CorrelationId}", correlationId);
+        }
     }
 }
