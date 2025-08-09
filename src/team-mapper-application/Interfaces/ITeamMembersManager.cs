74cb886c73660be00ecc592f18a9c9c6d04c9a34
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-09 16:50:50 +0200

    wip: skeleton added for TeamMembersManager

---
+++ src/team-mapper-application/Interfaces/ITeamMembersManager.cs
@@ -0,0 +1,8 @@
+﻿using team_mapper_domain.Models;
+
+namespace team_mapper_application.Interfaces;
+
+public interface ITeamMembersManager
+{
+    Task<IEnumerable<TeamMember>> GetAllTeamMembersAsync(Guid correlationId);
+}
---
+++ src/team-mapper-application/TeamMembersManager.cs
@@ -0,0 +1,12 @@
+﻿using team_mapper_application.Interfaces;
+using team_mapper_domain.Models;
+
+namespace team_mapper_application;
+
+public class TeamMembersManager : ITeamMembersManager
+{
+    public async Task<IEnumerable<TeamMember>> GetAllTeamMembersAsync(Guid correlationId)
+    {
+        throw new NotImplementedException();
+    }
+}
