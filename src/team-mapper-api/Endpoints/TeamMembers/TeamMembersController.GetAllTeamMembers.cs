f377d1cfbecafb2a10b9d014ce1797206868c87c
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-09 17:10:11 +0200

    wip: get all method implemented

---
+++ src/team-mapper-api/Endpoints/TeamMembers/TeamMembersController.GetAllTeamMembers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using team_mapper_api.Extensions;
+
+namespace team_mapper_api.Endpoints.TeamMembers
+{
+    public partial class TeamMembersController
+    {
+        [HttpGet("/workitems")]
+        public async Task<IActionResult> GetAllTeamMembersAsync()
+        {
+            var correlationId = Request.GetCorrelationId();
+            _logger.LogInformation("GetAllTeamMembersAsync Start. CorrelationId {@CorrelationId}", correlationId);
+
+            var results = await _teamMembersManager.GetAllTeamMembersAsync(correlationId);
+
+            _logger.LogInformation("GetAllTeamMembersAsync End. CorrelationId {@CorrelationId}", correlationId);
+
+            return Ok(results);
+        }
+    }
+}
