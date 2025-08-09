286f0e02c28545a3cee5c30f7385e99eeda124dd
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-09 16:46:24 +0200

    wip: teammemberscontroller added

---
+++ src/team-mapper-api/Endpoints/TeamMembers/TeamMembersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace team_mapper_api.Endpoints.TeamMembers;
+
+[Route("/teammembers")]
+[ApiController]
+public partial class TeamMembersController(ILogger<TeamMembersController> logger) : ControllerBase
+{
+    private ILogger<TeamMembersController> _logger { get; } = logger;
+}
