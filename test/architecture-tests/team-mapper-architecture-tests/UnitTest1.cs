0cdf30b0443e53045c9dff6e3e6fc0aa0b48d4e0
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-26 16:48:12 +0200

    wip: architecture test porject added

---
+++ test/architecture-tests/team-mapper-architecture-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace team_mapper_architecture_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
