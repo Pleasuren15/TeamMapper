e1a26c08b9cbd3da5e1610353525f3186d5b7adc
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-26 17:39:15 +0200

    wip: ICacheService skeleton added

---
+++ src/team-mapper-infrastructure/CacheService.cs
@@ -0,0 +1,21 @@
+﻿using team_mapper_infrastructure.Interfaces;
+
+namespace team_mapper_infrastructure;
+
+internal class CacheService : ICacheService
+{
+    public Task<T> GetRecordAsync<T>(string recordId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveRecordAsync(string recordId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetRecordAsync<T>(string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/team-mapper-infrastructure/Interfaces/ICacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace team_mapper_infrastructure.Interfaces;
+
+public interface ICacheService
+{
+    Task SetRecordAsync<T>(string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null, CancellationToken cancellationToken = default);
+    Task<T> GetRecordAsync<T>(string recordId, CancellationToken cancellationToken = default);
+    Task RemoveRecordAsync(string recordId, CancellationToken cancellationToken = default);
+}
