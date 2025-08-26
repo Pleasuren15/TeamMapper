b075b641dd04cc301240d67d39342055b2de7688
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-26 17:39:44 +0200

    wip: constructor updated

--- src/team-mapper-infrastructure/CacheService.cs
+++ src/team-mapper-infrastructure/CacheService.cs
@@ -1,8 +1,10 @@
-﻿using team_mapper_infrastructure.Interfaces;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using team_mapper_infrastructure.Interfaces;
 
 namespace team_mapper_infrastructure;
 
-internal class CacheService : ICacheService
+internal class CacheService(IDistributedCache cache, ILogger<CacheService> logger) : ICacheService
 {
     public Task<T> GetRecordAsync<T>(string recordId, CancellationToken cancellationToken = default)
     {
