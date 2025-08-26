1b0d382685487bd8cae50228cb7940c5f4b49e69
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-08-26 17:17:00 +0200

    wip: initial architecture tests added

---
+++ test/architecture-tests/team-mapper-architecture-tests/DomainTests.cs
@@ -0,0 +1,26 @@
+﻿using AwesomeAssertions;
+using NetArchTest.Rules;
+using team_mapper_domain.Models;
+
+namespace team_mapper_architecture_tests;
+
+public class Tests
+{
+    [TestCase(nameof(team_mapper_application))]
+    [TestCase(nameof(team_mapper_api))]
+    [TestCase(nameof(team_mapper_infrastructure))]
+    public void GivenDomainAssembly_ThenShouldNotDependOnAnyAssemblies(string _namespace)
+    {
+        // Arrange
+        var domainAssembly = typeof(WorkItem).Assembly;
+
+        // Act
+        var result = Types.InAssembly(domainAssembly)
+            .Should()
+            .NotHaveDependencyOn(_namespace)
+            .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
